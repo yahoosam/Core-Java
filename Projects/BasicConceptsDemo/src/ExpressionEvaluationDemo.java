171ecc30021bf3d84a76e6638ad2af673c7d7303
yahoosam <yahoosam@gmail.com>
2021-07-17 20:28:58 +0530

    minor comment fixes

--- Projects/BasicConceptsDemo/src/ExpressionEvaluationDemo.java
+++ Projects/BasicConceptsDemo/src/ExpressionEvaluationDemo.java
@@ -2,12 +2,12 @@ public class ExpressionEvaluationDemo {
 	public static void main(String[] args) {
 		String str = "abc";
 
-		int i =10;
-		int j =20;
-		int k =30;
-		
-		System.out.println(i+j+str+k);//30 + abc+30
-		System.out.println(str+i+j+k);//abc +60
-		System.out.println(i+str+j+k);//10 + abc+ 50
+		int i = 10;
+		int j = 20;
+		int k = 30;
+
+		System.out.println(i + j + str + k);// 30 + abc + 30
+		System.out.println(str + i + j + k);// abc + 102030
+		System.out.println(i + str + j + k);// 10 + abc + 2030
 	}
 }
