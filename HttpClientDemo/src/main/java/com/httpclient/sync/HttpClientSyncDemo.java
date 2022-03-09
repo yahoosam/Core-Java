a7e64bc23b14059cef98deb68bd348c48b312247
yahoosam <yahoosam@gmail.com>
2022-03-09 00:30:22 -0500

    Json to object mapping demo

--- HttpClientDemo/src/main/java/com/httpclient/sync/HttpClientSyncDemo.java
+++ HttpClientDemo/src/main/java/com/httpclient/sync/HttpClientSyncDemo.java
@@ -1,11 +1,17 @@
 package com.httpclient.sync;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HttpClientSyncDemo {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
@@ -14,6 +20,22 @@ public class HttpClientSyncDemo {
                 .GET().build();
         HttpResponse<String> response = httpClient.send(builder, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.statusCode());
-        System.out.println(response.body());
+        //System.out.println(response.body());
+
+        //convert json response to object
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Books> booklist = objectMapper.readValue(response.body(), new TypeReference<List<Books>>() {
+                })
+                .stream()
+                .filter(book -> (book.getId() >= 195))
+                .collect(Collectors.toList())
+                .stream()
+                .sorted(Comparator.comparing(Books::getId).reversed())
+                .collect(Collectors.toList());
+        //print the list
+        for (int i = 0; i < booklist.size(); i++) {
+            Books books = booklist.get(i);
+            System.out.println(books.getId());
+        }
     }
 }
